a4210e8080c08ce759445ba640ba4c2d6f69ddd4
Keshavpalde <keshavpalde01@gmail.com>
2026-09-04 14:38:36 +0530

    Fix diversity inclusion API populate

--- services/diversityInclusionPage.ts
+++ services/diversityInclusionPage.ts
@@ -7,7 +7,7 @@ const STRAPI_URL =
 
 export async function getDiversityInclusionPage(): Promise<DiversityInclusionPageData> {
   const res = await fetch(
-    `${STRAPI_URL}/api/diversity-and-inclusion?populate[diversityInclusionHero][populate]=*&populate[ourCommitmentToInclusion][populate][principles][populate]=*&populate[diversityInAction][populate][initiatives][populate]=*&populate[womenBrigade][populate][points][populate]=*&populate[womenBrigade][populate][Gif][populate]=*&populate[womenBrigade][populate][backgroundImage][populate]=*&populate[diversityStatistics][populate][statistics][populate]=*&populate[OurWorkplaceCulture][populate][slides][populate]=*`,
+    `${STRAPI_URL}/api/diversity-and-inclusion?populate[diversityInclusionHero][populate]=*&populate[ourCommitmentToInclusion][populate][principles][populate]=*&populate[diversityInAction][populate][initiatives][populate]=*&populate[womenBrigade][populate][points][populate]=*&populate[womenBrigade][populate][Gif][populate]=*&populate[diversityStatistics][populate][statistics][populate]=*&populate[OurWorkplaceCulture][populate][slides][populate]=*`,
     {
       next: {
         revalidate: 60,
